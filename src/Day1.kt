f8e2ba91533fe04a731fb70ff98b6d65aaf93e95
Niamh Power <niamh@apposing.co.uk>
2016-12-13 13:49:22 +0000

    setup day 1 class

---
+++ src/Day1.kt
@@ -0,0 +1,28 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+import org.testng.Assert
+import org.testng.annotations.Test
+
+/**
+ * Created by Niamh on 13/12/2016.
+ */
+
+class day1 {
+    @Test
+    fun simpleTest() {
+        Assert.assertEquals(2, 1+1)
+    }
+
+
+    fun main(args: Array<String>) {
+        //val input = Files.newBufferedReader(Paths.get("data\\day1_input.txt")).readLine()
+
+
+
+    }
+}
+
+
+
+
+
